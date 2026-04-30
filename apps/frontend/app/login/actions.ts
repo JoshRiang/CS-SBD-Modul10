29c4dfd02e37a984dd0542476f1e0a67e7a1ce71
josh_riang <sandinya1sampai8@gmail.com>
2026-04-30 23:19:33 +0700

    Updating cors settings in frontend

--- apps/frontend/app/login/actions.ts
+++ apps/frontend/app/login/actions.ts
@@ -4,6 +4,6 @@ import { cookies } from "next/headers";
 
 export async function setLoginSession(token: string, userDetailStr: string) {
   const cookieStore = await cookies();
-  cookieStore.set("session_key", token, { secure: true });
-  cookieStore.set("user_detail", userDetailStr);
+  cookieStore.set("session_key", token, { secure: true, path: "/", sameSite: "lax" });
+  cookieStore.set("user_detail", userDetailStr, { secure: true, path: "/", sameSite: "lax" });
 }
